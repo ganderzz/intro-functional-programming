73c3236fb61733372fbbd0cff41e395cbfdef72d
Dylan Paulus <dylantpaulus@gmail.com>
2019-02-13 20:11:31 -0800

    Switch slide title

--- src/slides/1-intro.jsx
+++ src/slides/1-intro.jsx
@@ -5,14 +5,13 @@ const notes = `
   Introduction<br />
   - Name<br />
   - Title & Company<br /><br />
-  We will be looking at easy wins to make React fast
 `;
 
 export default function() {
   return (
     <Slide bgColor="primary" textAlign="center" notes={notes}>
-      <Heading size={1} textColor="secondary" fit caps lineHeight={1.2}>
-        Making {"<React />"} Wicked Fast
+      <Heading size={2} textColor="secondary" fit caps lineHeight={1.2}>
+        Introduction to Functional Programming
       </Heading>
       <Text bold textColor="tertiary" textSize="2rem">
         Dylan Paulus
